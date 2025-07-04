7e7b687230ab6341bbb6e96a1f2e5bda66cb5652
jerald <jeraldlabalan26@gmail.com>
2025-07-04 16:51:55 +0800

    chore: display review

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product\Product;
+use App\Models\Product\Review;
+
 
 class HomeController extends Controller
 {
@@ -27,8 +29,10 @@ class HomeController extends Controller
         // Fetch the latest 4 products from the database
         $products = Product::select()->orderBy('id', 'desc')->take('4')->get();
 
-        // Return the view with the products data
-        return view('home', compact('products'));
+        $reviews = Review::select()->orderBy('id', 'desc')->take('4')->get();
+
+        // Return the view with the products and reviews data
+        return view('home', compact('products', 'reviews'));
 
     }
 }
