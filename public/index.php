020b1b49aeaa4f00d14ec6b5de699bb028ab3111
jerald <jeraldlabalan26@gmail.com>
2025-07-12 21:08:16 +0800

    edit index.php

--- public/index.php
+++ public/index.php
@@ -17,4 +17,9 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$response = $app->handle(
+    $request = Request::capture()
+)->send();
+
+$app->terminate($request, $response);
+
